3609ee71d93f173c6b493e9eecbeece7b80367b6
Mickenpox <robhensley10@gmail.com>
2019-03-03 14:50:13 -0800

    Update block-redirects.js

--- extention/js/block-redirects.js
+++ extention/js/block-redirects.js
@@ -1,3 +1,5 @@
-var header = document.createElement('h1');
-header.textContent = "This page has been eaten";
-document.body.appendChild(header);
+//var header = document.createElement('h1');
+//header.textContent = "This page has been eaten";
+//document.body.appendChild(header);
+
+document.getElementById('h1').outerHTML = "U R Hakd";
